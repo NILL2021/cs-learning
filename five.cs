04a9c18146c7608788f9144ea1afa135dc786072
ToastersUnited <81653971+NILL2021@users.noreply.github.com>
2022-02-16 20:32:42 +0000

    Create five.cs

---
+++ five.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace CsTut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool age = false;
+            if (age == true) {
+                Console.WriteLine("your true bruv");
+            }
+            else if (age == false) {
+                Console.WriteLine("yOU ARE FalsE");
+            }
+            else {
+                Console.WriteLine("Man just broke C#");
+            }
+            Console.Beep();
+            Console.ReadKey();
+        }
+    }
+}
