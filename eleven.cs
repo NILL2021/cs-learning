bb73f9055dc4e3338407e55d8ce17a66ad4c5a98
ToastersUnited <81653971+NILL2021@users.noreply.github.com>
2022-02-17 16:00:56 +0000

    Create eleven.cs

---
+++ eleven.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace randnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String firstName = "Timmy";
+            String lastName = "Jimmingson";
+
+            Console.WriteLine($"Hello {firstName} {lastName}!");            
+        }
+    }
+}
